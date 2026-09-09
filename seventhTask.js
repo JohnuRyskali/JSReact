ac676d63d49c6c1f8b33101e290aa30f0bd4708f
Zhanarys <rzhanarys@gmail.com>
2026-09-09 16:11:22 +0500

    Initial empty commit

--- seventhTask.js
+++
@@ -1,9 +0,0 @@
-{
-const add = (a, b) => a + b;
-const multiply = (a, b) => a * b;
-
-const calculate = (a, b, operation) => operation(a, b);
-
-console.log(calculate(5, 3, add));
-console.log(calculate(5, 3, multiply));
-}
